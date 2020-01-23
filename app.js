723d3203a4e073b26a5a1936ad7cb80fd23c28f2
Regan Loper <reganloper@Regans-MacBook-Pro.local>
2020-01-23 17:57:22 -0600

    added resume object

---
+++ app.js
@@ -0,0 +1,48 @@
+let resume = {
+    name: "Regan Loper",
+    career: "Attorney",
+    description: "I am a minimalist.",
+    interests =[
+        "Golf", "Software Development", "Alternative Forms of Transportation",
+        "Playing Guitar"
+    ],
+    priorExperience =[
+        {
+            company: "Burr & Forman, LLP",
+            jobTitle: "Associate Attorney",
+            jobDescription: "Bankruptcy attorney in Burr's creditors' rights and bankruptcy practice group."
+        },
+        {
+            company: "Burr & Forman, LLP",
+            jobTitle: "Summer Associate",
+            jobDescription: "Employed as legal associate to attorneys in law firm after my first and second years of law school."
+        },
+        {
+            company: "Washington & Lee University School of Law",
+            jobTitle: "Summer Intern",
+            jobDescription: "Interned at law school's Black Lung Clinic program, and served as attorney assisting ex-coal miners in obtaining black lung disability benefits."
+        }
+    ],
+    skills: [
+        {
+            name: "Golf",
+            isCool: false
+        },
+        {
+            name: "Java",
+            isCool: true
+        },
+        {
+            name: "Playing Guitar",
+            isCool: true
+        },
+        {
+            name: "Personal Finance",
+            isCool: false
+        },
+        {
+            name: "Investing",
+            isCool: false
+        }
+    ]
+}
