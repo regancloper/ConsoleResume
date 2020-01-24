ecfa2c6ec509b1d734f797bb1683bd2362177d72
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-01-23 21:10:00 -0600

    added all js code for program

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-let resume = {
+let basicInfo = {
     name: "Regan Loper",
     career: "Attorney",
-    description: "I am a minimalist.",
-    interests =[
+    description: "I am a minimalist."
+}
+
+let otherInfo = {
+    interests: [
         "Golf", "Software Development", "Alternative Forms of Transportation",
         "Playing Guitar"
     ],
-    priorExperience =[
+    priorExperience: [
         {
             company: "Burr & Forman, LLP",
             jobTitle: "Associate Attorney",
@@ -45,4 +48,48 @@ let resume = {
             isCool: false
         }
     ]
+}
+
+// prints basic info to the console
+for (let i in basicInfo) {
+    let key = i.charAt(0).toUpperCase() + i.slice(1);
+    let val = basicInfo[i];
+    if (i == "name") {
+        val = val.toUpperCase();
+    }
+    console.log(key + ": " + val);
+}
+console.log("\n");
+
+// prints interests to the console
+console.log("My Interests:");
+for (let i = 0; i < otherInfo.interests.length; i++) {
+    console.log("* " + otherInfo.interests[i]);
+}
+console.log("\n");
+
+// prints prior job experience to the console
+console.log("My Previous Experience:");
+for (let i = 0; i < otherInfo.priorExperience.length; i++) {
+    displayPosition(otherInfo.priorExperience[i].company, otherInfo.priorExperience[i].jobTitle, otherInfo.priorExperience[i].jobDescription);
+}
+console.log("\n");
+
+// prints skills to the console
+console.log("My Skills:");
+for (let i = 0; i < otherInfo.skills.length; i++) {
+    displaySkill(otherInfo.skills[i].name, otherInfo.skills[i].isCool);
+}
+
+// displays prior positions at other companies
+function displayPosition(comp, title, desc) {
+    console.log("* " + title + " at " + comp + " - " + desc);
+}
+
+// displays skills
+function displaySkill(name, coolness) {
+    if (coolness == true) {
+        name = "BAM: " + name;
+    }
+    console.log("* " + name);
 }
